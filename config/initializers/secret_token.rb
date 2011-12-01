f16d9743e95d2f9c8e63c290b50e4bc0916ad0e0
Jordan Betteridge <jordandbetteridge@gmail.com>
2011-11-30 21:28:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrlDictionary::Application.config.secret_token = '3d8c8309e998d9ca118ebbeb5ec66f47c386ecfe5393a8d78d84dfd2c09180a2a23a419cc088e32af45b2137daed9bd1fe3798ea3f9a36292958d39d096ef381'
